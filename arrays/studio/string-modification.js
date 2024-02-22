7d1952c23bc0f9466ded641c8e51c2e95b62292c
programmingwithmalik <malikblassingale@gmail.com>
2024-02-21 21:38:44 -0500

    string studio, completed!

--- arrays/studio/string-modification.js
+++ arrays/studio/string-modification.js
@@ -1,6 +1,6 @@
 const input = require('readline-sync');
 let str = "LaunchCode";
-let info = input.question("Choose a number of letters to re-arrange");
+
 
 //1) Use string methods to remove the first three characters from the string and add them to the end.
 let newStr =str.slice(info) + str.slice(0,info)
@@ -10,6 +10,8 @@ console.log(newStr);
 //Use a template literal to print the original and modified string in a descriptive phrase.
 
 //2) Modify your code to accept user input. Query the user to enter the number of letters that will be relocated.
+
+let info = input.question("Choose a number of letters to re-arrange");
 
 //3) Add validation to your code to deal with user inputs that are longer than the word. In such cases, default to moving 3 characters. Also, the template literal should note the error.
 if (info >str.length) {

c3d39d98aff4b603e225aaaa08302ca32cdd5526
programmingwithmalik <55502902+programmingwithmalik@users.noreply.github.com>
2024-02-26 18:25:55 -0500

    Update script.js

--- dom-and-events/exercises/script.js
+++ dom-and-events/exercises/script.js
@@ -4,7 +4,25 @@ function init () {
     const paragraph = document.getElementById("statusReport");
 
     // Put your code for the exercises here.
-    
+    button.addEventListener('click', event => {
+   paragraph.innerHTML = 'Houston! We have liftoff!';
+});
+
+    button.addEventListener('mouseover', function (event) {
+
+    })
+
+    missionAbort.addEventListener("mouseover", function( event ) {
+   event.target.style.backgroundColor = "red";
+});
+
+    missionAbort.addEventListener("mouseout", function( event ) {
+   event.target.style.backgroundColor = "";
+});
+
+    button.addEventListener('click', event => {
+   paragraph.innerHTML = 'Houston! We have liftoff!';
+});
 }
 
 window.addEventListener("load", init);

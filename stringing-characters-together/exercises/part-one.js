27281f36e38e4bfc211ff6f3aaf563287888e6be
programmingwithmalik <malikblassingale@gmail.com>
2024-03-18 21:04:49 -0400

    part two exercise, redid

--- stringing-characters-together/exercises/part-one.js
+++ stringing-characters-together/exercises/part-one.js
@@ -1,10 +1,17 @@
 let num = 1001;
 
 //Returns 'undefined'.
+
 console.log(num.length);
 
 //Use type conversion to print the length (number of digits) of an integer.
-
+num = String(num);
+console.log(num);
+console.log(num.length);
 //Follow up: Print the number of digits in a DECIMAL value (e.g. num = 123.45 has 5 digits but a length of 6).
-
+if (String(num).includes(".")){
+    console.log(num.length-1);
+} else{
+    console.log(num.length);
+}
 //Experiment! What if num could be EITHER an integer or a decimal?  Add an if/else statement so your code can handle both cases.
--- stringing-characters-together/exercises/part-three.js
+++ stringing-characters-together/exercises/part-three.js
@@ -4,10 +4,11 @@ let language = 'JavaScript';
 
 //1. Use string concatenation and two slice() methods to print 'JS' from 'JavaScript'
 
+console.log(language.slice(0,1)+language.slice(4,5));
 //2. Without using slice(), use method chaining to accomplish the same thing.
 
 //3. Use bracket notation and a template literal to print, "The abbreviation for 'JavaScript' is 'JS'."
-
+console.log(`The abbreviation for '${language}' is '${language[0]+language[4]}'.`);
 //4. Just for fun, try chaining 3 or more methods together, and then print the result.
 
 //Part Three section Two

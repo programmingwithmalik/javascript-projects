4db47be88b228d6ced331744308deac57ec2599e
programmingwithmalik <malikblassingale@gmail.com>
2024-03-21 16:08:52 -0400

    pt 2

--- objects-and-math/studio/ObjectsStudio02.js
+++ objects-and-math/studio/ObjectsStudio02.js
@@ -1,14 +1,35 @@
 // Code your orbitCircumference function here:
+function calculateCircumference(radius) {
+    const EARTH_RADIUS = 6371; // Earth's average radius in km
+    let totalRadius = EARTH_RADIUS + radius;
+    return Math.round(2 * Math.PI * totalRadius);
+}
+
 
 
 // Code your missionDuration function here:
+function missionDuration(numberOfOrbits, radius = 2000, speed = 28000) {
+    let orbitCircumference = calculateCircumference(radius);
+    let totalDistance = orbitCircumference * numberOfOrbits;
+    let timeHours = totalDistance / speed;
+    return timeHours.toFixed(2);
+}
 
 
 // Copy/paste your selectRandomEntry function here:
-
+function selectRandomEntry(idArray) {
+    let randomIndex = Math.floor(Math.random() * idArray.length);
+    return idArray[randomIndex];
+}
 
 // Code your oxygenExpended function here:
+function oxygenExpended(candidate, radius, speed) {
+    let spacewalkDuration = missionDuration(3, radius, speed); // Duration for 3 orbits
+    let oxygenConsumed = candidate.o2Used(spacewalkDuration);
+    oxygenConsumed = parseFloat(oxygenConsumed.toFixed(3)); // Round to 3 decimal places
 
+    return `${candidate.name} will perform the spacewalk, which will last ${spacewalkDuration} hours and require ${oxygenConsumed} kg of oxygen.`;
+}
 
 // Candidate data & crew array.
 let candidateA = {
